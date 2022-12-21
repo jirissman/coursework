8d0aac00d2a071c9a0c44c8bf5c296b8ef98aaaf
Joseph Rissman <rissman.jo@husky.neu.edu>
2022-12-21 12:15:07 -0500

    Add files via upload
    CS231 Files

---
+++ BU/CS231/hello.cpp
@@ -0,0 +1,13 @@
+// hello.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// My first C++ program!
+// Name: Joseph Rissman
+// Date: 9-13-2022
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
