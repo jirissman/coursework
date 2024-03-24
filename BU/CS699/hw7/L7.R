387a4b4efa83ed493354f0c70e06e66238dcdc25
Joseph Rissman <jirissman@gmail.com>
2024-03-24 12:39:46 -0400

    CS699 Homework 7

---
+++ BU/CS699/hw7/L7.R
@@ -0,0 +1,289 @@
+#######################################################################
+## association rules
+#######################################################################
+library(arules)
+library(arulesViz)
+library(tidyverse)
+library(dplyr)
+library(mlbench)
+
+setwd('C:/Courses/C2024/699/Slides/L7')
+
+## small baskets 1
+tdb<-read.transactions('small-baskets-1.csv', sep = ",")
+summary(tdb)
+colnames(tdb)
+as(tdb, "matrix")
+image(tdb)
+inspect(tdb)
+itemFrequency(tdb)
+
+# plot the frequency of items
+itemFrequencyPlot(tdb)
+itemFrequencyPlot(tdb, support = 0.5)
+itemFrequencyPlot(tdb, topN = 3)
+
+
+# mine frequent itemsets
+# default: min. support = 0.1, min. confidence = 0.8, maxlen = 10. maxtime = 5 sec
+L <- apriori(tdb, parameter = list(support =0.3), target = "frequent itemsets")
+summary(L)
+inspect(L)
+L.sorted <- sort(L, by = "support")
+inspect(L.sorted)
+
+# X is maximal frequent itemset if there is no proper superset Y that is frequent
+# X is a closed itemset if there is not proper superset Y s.t. sup(X) = sup(Y)
+# X is a closed frequent itemset if X is closed and frquent
+# all maximal frequent itemsets are also closed 
+# not all closed frequent itemsets are maximal
+L.max <- L[is.maximal(L)]
+inspect(L.max)
+L.closed <- L[is.closed(L)]
+inspect(L.closed)
+
+L_df <- as(L, "data.frame")
+L_df[L_df$items=="{Milk}", ]
+
+
+#######################################################################
+## small baskets 2
+tdb<-read.transactions('small-baskets-2.csv', sep = ",")
+summary(tdb)
+as(tdb, "matrix")
+image(tdb)
+inspect(tdb)
+itemFrequency(tdb)
+
+# plot the frequency of items
+itemFrequencyPlot(tdb)
+itemFrequencyPlot(tdb, support = 0.5)
+itemFrequencyPlot(tdb, topN = 3)
+
+# mine frequent itemsets
+L <- apriori(tdb, parameter = list(support =0.3, target = "frequent itemsets"))
+summary(L)
+inspect(L)
+inspect(sort(L, by = "support")[1:5])
+L_df <- as(L, "data.frame")
+L_df[L_df$items=="{milk}", ]
+
+# mine rules
+# apriori (in arules package) only generates rules with one item in RHS
+rules <- apriori(tdb, parameter = list(support =0.3, 
+                                       confidence = 0.2, minlen = 2),
+                 target = "rules")
+summary(rules)
+inspect(rules)
+quality(rules)
+
+# sorting rules by confidence
+inspect(sort(rules, by = "confidence"))
+
+# finding subsets of rules containing any beer items
+beerrules <- subset(rules, items %in% "beer")
+inspect(beerrules)
+
+# write rules to a CSV file
+write(rules, file = "small-baskets-2-rules.csv",
+      sep = ",", quote = TRUE, row.names = FALSE)
+
+# convert rules to data frame
+rules_df <- as(rules, "data.frame")
+
+# add cosine, chi-square, and Kulczynski measures
+rules_df$chiSquared <- interestMeasure(rules, "chiSquared")
+rules_df$cosine <- interestMeasure(rules, "cosine")
+rules_df$kulczynski <- interestMeasure(rules, "kulczynski")
+sapply(rules_df, class)
+head(rules_df)
+rules_df
+corrMatrix <- cor(rules_df[c(5,8,9)])
+corrMatrix
+
+##########################################################################
+## L7 exercise
+tdb<-read.transactions('L7-ex.csv', sep = ",")
+summary(tdb)
+inspect(tdb)
+itemFrequency(tdb)
+
+# plot the frequency of items
+itemFrequencyPlot(tdb)
+itemFrequencyPlot(tdb, topN = 5)
+
+# a visualization of the sparse matrix for the first five transactions
+image(tdb[1:5])
+
+# mine frequent itemsets
+L <- apriori(tdb, parameter = list(support =0.3), target = "frequent itemsets")
+summary(L)
+inspect(L)
+inspect(sort(L, by = "support")[1:5])
+L_df <- as(L, "data.frame")
+L_df[L_df$items=="{5}", ]
+
+# mine rules
+rules <- apriori(tdb, parameter = list(support =0.3, 
+                                       confidence = 0.8, minlen = 2),
+                 target = "rules")
+summary(rules)
+inspect(rules)
+
+# sorting rules by confidence
+inspect(sort(rules, by = "confidence")[1:5])
+
+# finding subsets of rules containing any berry items
+beerrules <- subset(rules, items %in% "5")
+inspect(beerrules)
+
+# write rules to a CSV file
+write(rules, file = "L7-ex-rules.csv",
+      sep = ",", quote = TRUE, row.names = FALSE)
+
+# convert rules to data frame
+rules_df <- as(rules, "data.frame")
+# add cosine, chi-square, and Kulczynski measures
+rules_df$chiSquared <- interestMeasure(rules, "chiSquared")
+rules_df$cosine <- interestMeasure(rules, "cosine")
+rules_df$kulczynski <- interestMeasure(rules, "kulczynski")
+rules_df
+corrMatrix <- cor(rules_df[c(5,8,9)])
+corrMatrix
+
+##########################################################################
+# groceries
+# dataset is from arules package
+
+tdb<-read.transactions('groceries.csv', sep = ",")
+tdb
+summary(tdb)
+
+L <- apriori(tdb, parameter = list(support =0.01), target = "frequent itemsets")
+summary(L)
+inspect(L)
+L.sorted <- sort(L, by = "support")
+inspect(L.sorted)
+
+# X is maximal frequent itemset if there is no proper superset Y that is frequent
+# X is a closed itemset if there is not proper superset Y s.t. sup(X) = sup(Y)
+# X is a closed frequent itemset if X is closed and frquent
+# all maximal frequent itemsets are also closed 
+# not all closed frequent itemsets are maximal
+L.max <- L[is.maximal(L)]
+inspect(L.max)
+L.closed <- L[is.closed(L)]
+inspect(L.closed)
+
+rules <- apriori(tdb, parameter = list(support =0.01, 
+                                       confidence = 0.2, minlen = 2),
+                 target = "rules")
+summary(rules)
+inspect(sort(rules, by = "confidence")[1:10])
+inspect(sort(rules, decreasing = FALSE, by = "confidence")[1:5])
+inspect(sort(rules, by = "support")[1:5])
+inspect(sort(rules, decreasing = FALSE, by = "support")[1:5])
+
+# convert rules to data frame
+rules_df <- as(rules, "data.frame")
+# add cosine, chi-square, and Kulczynski measures
+rules_df$chiSquared <- interestMeasure(rules, "chiSquared")
+rules_df$cosine <- interestMeasure(rules, "cosine")
+rules_df$kulczynski <- interestMeasure(rules, "kulczynski")
+measures <- rules_df[, -c(2,4,5,6)]
+sorted_by_confidence <- measures[order(-measures$confidence), ]
+sorted_by_confidence[1:20, ]
+
+# plot scatter plot: support vs. confidence
+plot(rules)
+
+
+#########################################################################
+## weather
+weather <- read.csv('weather_nominal.csv')
+sapply(weather, class)
+all <- 1:ncol(weather)
+weather[ , all] <- lapply(weather[ , all], as.factor)
+weather.tr <- transactions(weather)
+summary(weather.tr)
+
+L <- apriori(weather.tr, parameter = list(support =0.2), target = "frequent itemsets")
+summary(L)
+inspect(L)
+L.sorted <- sort(L, by = "support")
+inspect(L.sorted)
+
+rules <- apriori(weather.tr, supp = 0.2, conf = 0.7, target = "rules")
+summary(rules)
+inspect(rules, n = length(rules), by = "confidence")
+
+#############################################################################
+## recommendation
+#############################################################################
+
+library(recommenderlab)
+
+load(file = "MovieLensRatings.rda")
+load(file = "MovieLensMovies.rda")
+
+ratings <- MovieLensRatings
+movies <- MovieLensMovies
+head(ratings)
+head(movies)
+
+# convert ratings to rating matrix
+idxUserId <- sort(unique(ratings$userId))
+idxMovieId <- sort(unique(ratings$movieId))
+m <- matrix(NA, nrow=length(idxUserId), ncol=length(idxMovieId),
+            dimnames=list(
+              user=paste("u", 1:length(idxUserId), sep=''),
+              item=movies$title[match(idxMovieId, movies$movieId)]
+            ))
+
+for (i in 1:nrow(ratings)) {
+  rating <- ratings[i,]
+  irow <- match(rating$userId, idxUserId)
+  icol <- match(rating$movieId, idxMovieId)
+  m[irow, icol] <- rating$rating
+}
+dim(m)
+
+# convert to realRatingMatrix type
+ratingMatrix <- as(m, "realRatingMatrix")
+
+# use only movies with more than 100 ratings
+Movie100 <- ratingMatrix[rowCounts(ratingMatrix) > 100, ]
+
+# UBCF model and prediction
+ubcf_recommender <- Recommender(Movie100[-1], method="UBCF")
+# recommend top 5 movies for user 1
+pred <- predict(ubcf_recommender, Movie100[1], n = 5)
+as(pred, 'list')
+
+# IBCF model and prediction
+ibcf_recommender <- Recommender(Movie100[-1], method="IBCF")
+# recommend top 5 movies for user 1
+pred <- predict(ibcf_recommender, Movie100[1], n = 5)
+as(pred, 'list')
+
+##########################################################################
+## evaluation
+
+set.seed(31)
+
+## evaluated by how well it is able to predict randomly chosen 10 items.
+e <- evaluationScheme(Movie100, method="split", train=0.8, given=10)
+
+r1 <- Recommender(getData(e, "train"), "UBCF")
+r2 <- Recommender(getData(e, "train"), "IBCF")
+r3 <- Recommender(getData(e, "train"), "RANDOM")
+
+p1 <- predict(r1, getData(e, "known"), type="ratings")
+p2 <- predict(r2, getData(e, "known"), type="ratings")
+p3 <- predict(r3, getData(e, "known"), type="ratings")
+error <- rbind(
+  UBCF = calcPredictionAccuracy(p1, getData(e, "unknown")),
+  IBCF = calcPredictionAccuracy(p2, getData(e, "unknown")),
+  RANDOM = calcPredictionAccuracy(p3, getData(e, "unknown"))
+)
+error
